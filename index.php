66fd95cb9a6bd021e67740996a50f713c9f60c0f
Lucas Cherkewski <lucas@ecustom.ca>
2015-03-20 14:32:49 -0400

    Work on frontpage (using index.php as a working zone)

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 	<header class="site-intro">
 		<h1 class="site-intro__header">We’re <strong>Crossing Borders</strong>, a group dedicated to breaking down barriers by sharing the stories of both newcomer and Canadian youth across Canada.</h1>
 
-		<p class="site-intro__byline"></p>
+		<p class="site-intro__byline">We run events across the country to tell our own stories—and we provide educational materials to help you do the same.</p>
+
+		<a href="#">Learn about our events</a>
+		<a href="#">Run your own event</a>
 	</header>
 </main>
 
