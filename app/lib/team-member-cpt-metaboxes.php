7a5fafc93b615f6f73da4ff45ff487645fda46a9
Lucas Cherkewski <lucas@ecustom.ca>
2015-04-19 12:45:17 -0400

    update metaboxes on team members to include bio

--- app/lib/team-member-cpt-metaboxes.php
+++ app/lib/team-member-cpt-metaboxes.php
@@ -11,7 +11,7 @@
 /**
  * Add custom author metabox (via cmb2 library)
  */
-// add_action( 'cmb2_init', 'cb_add_team_member_metabox' );
+add_action( 'cmb2_init', 'cb_add_team_member_metabox' );
 
 function cb_add_team_member_metabox() {
 
@@ -28,19 +28,48 @@ function cb_add_team_member_metabox() {
 		'show_names'    => true, // Show field names on the left
 	) );
 
-	// Add author text field
 	$metabox->add_field( array(
-		'name'       => __( 'Custom Title' ),
-		'desc'       => __( 'If the contributor has a special title (like editor or designer), enter it here.', 'cb' ),
+		'name'       => __( 'Special Title', 'cb' ),
+		'desc'       => __( 'If the team member has a special title (like “singer” or “teacher”), enter it here.', 'cb' ),
 		'id'         => $prefix . 'title',
 		'type'       => 'text',
 	) );
 
 	$metabox->add_field( array(
-		'name'       => __( 'Special Contributor', 'cb' ),
-		'desc'       => __( 'If this person is a special contributor who deserves to appear before the others in the list, check this box.', 'cb' ),
-		'id'         => $prefix . 'special',
-		'type'       => 'checkbox',
+		'name'             => __( 'Bio Post', 'cb' ),
+		'desc'             => __( 'If a bio/interview post has been written for this team member, select it from this list! (Don’t forget to set the category of the post to “Team Member Interview” to ensure it shows up here.)', 'cb' ),
+		'id'               => $prefix . 'bio_post_id',
+		'type'             => 'select',
+		'show_option_none' => true,
+		'options'          => 'cb_list_team_member_bio_posts',
 	) );
+
+}
+
+function cb_list_team_member_bio_posts() {
+
+	$team_member_bio_posts = array();
+
+	$team_member_bio_posts_query = new WP_Query( array(
+		'post_status'   => 'published',
+		'category_name' => 'team-member-bio',
+		'orderby'       => 'modified',
+	) );
+
+	if ( $team_member_bio_posts_query->have_posts() ) {
+
+		while ( $team_member_bio_posts_query->have_posts() ) {
+
+			$team_member_bio_posts_query->the_post();
+
+			$team_member_bio_posts[ get_the_id() ] = get_the_title();
+
+		}
+
+		wp_reset_postdata();
+
+	}
+
+	return $team_member_bio_posts;
 
 }
